30ca4a32486d1bc481190c7e230b799e3c04770b
volkrav <volodymyr.kravchenko@icloud.com>
2022-08-29 21:47:49 +0300

    Рефакторинг функції видалення повідомлень

--- app/handlers/order.py
+++ app/handlers/order.py
@@ -9,7 +9,7 @@ from app.services.utils import delete_inline_keyboard
 async def command_view_order(message: types.Message):
     chat_id = CURRENT_ID['chat_id']
     message_id = CURRENT_ID['message_id']
-    await delete_inline_keyboard(message, chat_id, message_id)
+    await delete_inline_keyboard(message, CURRENT_ID)
 
     answer = (
               f'Тут буде виведене ваше замовлення, яке можна буде:\n'
--- app/services/utils.py
+++ app/services/utils.py
@@ -3,8 +3,7 @@ from aiogram import types
 
 async def delete_inline_keyboard(message: types.Message, current_ids: dict):
 
-    for current_id in current_ids:
-        await message.bot.delete_message(
-            chat_id=current_id['chat_id'],
-            message_id=current_id['message_id']
-        )
+    await message.bot.delete_message(
+        chat_id=current_ids['chat_id'],
+        message_id=current_ids['message_id']
+    )
