f65331b4da3c5b92507465b02ead74eb69605392
volkrav <volodymyr.kravchenko@icloud.com>
2022-08-29 21:42:27 +0300

    Рефакторинг функції виделаення повідомлень

--- app/services/utils.py
+++ app/services/utils.py
@@ -1,5 +1,10 @@
 from aiogram import types
 
 
-async def delete_inline_keyboard(message: types.Message, chat_id: int, msg_id: int):
-    await message.bot.delete_message(chat_id=chat_id, message_id=msg_id)
+async def delete_inline_keyboard(message: types.Message, current_ids: dict):
+
+    for current_id in current_ids:
+        await message.bot.delete_message(
+            chat_id=current_id['chat_id'],
+            message_id=current_id['message_id']
+        )
